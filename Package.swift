4c281beedfc049c17449bed1911fa48939ed064d
blostemiossdk <aditya.prasad@blostem.com>
2026-08-17 12:04:57 +0530

    Release blostemSDK 1.0.18

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "blostemSDK",
-            url: "https://github.com/adityaprasad-sys/blostemSDK-SPM/releases/download/1.0.17/blostemSDK.xcframework.zip",
-            checksum: "42c301a0cad2fd95e5b50f4cf0edc7fbd192fdc26c85f62e1632219a845941b8"
+            url: "https://github.com/adityaprasad-sys/blostemSDK-SPM/releases/download/1.0.18/blostemSDK.xcframework.zip",
+            checksum: "e2798c4a0596445b71c7477109ca381965f8044d7afe40f406812bda6b4c493b"
         )
     ]
 )
